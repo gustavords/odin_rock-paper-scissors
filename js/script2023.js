9e4a0779c11d3fa38b5e6806b3b9feebd33b7b79
gustavords <gustavords92@gmail.com>
2023-04-16 19:58:02 -0400

    Basic game logic established for 5 rounds

--- js/script2023.js
+++ js/script2023.js
@@ -1,8 +1,8 @@
 
-console.log(playRound(`rock`));
-console.log(playRound(`PAPER`));
-console.log(playRound(`scisSor`));
-
+// console.log(playRound(`rock`));
+// console.log(playRound(`PAPER`));
+// console.log(playRound(`scisSor`));
+game();
 
 function getComputerChoice() {
     let range = 3; //0,1,2
@@ -21,7 +21,8 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection = getComputerChoice()) {
 
     let winningChoice;
-    let result = `player: ${playerSelection}\ncomp: ${computerSelection}\n`;
+    let result;
+    let consoleMessage = `player: ${playerSelection}\ncomp: ${computerSelection}\n`;
     let pS = playerSelection.toLowerCase();
     playerSelection = pS;
 
@@ -41,15 +42,43 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     }
 
     if (playerSelection == winningChoice) {
-        result += `player (${playerSelection}) has WON.`;
+        consoleMessage += `player (${playerSelection}) has WON.`;
+        result = `player`;
     }
     else if (winningChoice == `tie`) {
-        result += `player (${playerSelection}) and comp (${computerSelection}) tied.`;
+        consoleMessage += `player (${playerSelection}) and comp (${computerSelection}) tied.`;
+        result = `tie`;
     }
     else {
-        result += `comp (${computerSelection}) has WON,`;
+        consoleMessage += `comp (${computerSelection}) has WON,`;
+        result = `comp`;
     }
+
+    console.log(consoleMessage);
 
     return result;
 
 }
+
+function game() {
+    let compScore = 0,
+        playerScore = 0,
+        tiePoints = 0,
+        playerSelection = ``;
+
+    let x = 0
+    while (x < 5) {
+        playerSelection = prompt(`Type in either: Rock, Paper or Scissor`, ``);
+        // if(playerSelection === null || playerSelection == `` || playerSelection == undefined){
+        //     playerSelection = prompt(`Try Again\nType in either: Rock, Paper or Scissor`, ``);
+        // }
+        let points = playRound(playerSelection);
+        
+        if(points == `player`) playerScore += 1;
+        else if(points == `comp`) compScore += 1;
+        else tiePoints += 1;
+        console.log(`compScore:${compScore}\nplayerScore:${playerScore}\ntieScore:${tiePoints}\n`);
+        x++;
+    }
+}
+
