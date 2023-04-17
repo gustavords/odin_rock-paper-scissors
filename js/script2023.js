98efdcc048a85d0fe4e5813ca1544e75aab15a26
gustavords <gustavords92@gmail.com>
2023-04-17 01:23:03 -0400

    Update game logic for UI for 5 rounds

--- js/script2023.js
+++ js/script2023.js
@@ -1,8 +1,7 @@
 
-// console.log(playRound(`rock`));
-// console.log(playRound(`PAPER`));
-// console.log(playRound(`scisSor`));
-game();
+let compScore = 0,
+    playerScore = 0,
+    tiePoints = 0;
 
 function getComputerChoice() {
     let range = 3; //0,1,2
@@ -55,40 +54,52 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     }
 
     console.log(consoleMessage);
+    appendRound(consoleMessage);
 
     return result;
+}
+
+function game(playerSelection) {
+    let points = playRound(playerSelection);
+
+    if (points == `player`) playerScore += 1;
+    else if (points == `comp`) compScore += 1;
+    else tiePoints += 1;
+
+    //why does the switch not work??
+    // switch (points = true) {
+    //     case (points == `player`):
+    //         playerScore += 1;
+    //         break;
+    //     case (points == `comp`):
+    //         compScore += 1;
+    //         break;
+    //     default:
+    //         tiePoints += 1;
+    // }
+
+    console.log(`compScore:${compScore}\nplayerScore:${playerScore}\ntieScore:${tiePoints}\n`);
+    appendScore(`compScore:${compScore}\nplayerScore:${playerScore}\ntieScore:${tiePoints}\n`);
+
+    //logic set for 5 rounds
+    //two separate if-statements for readability
+    if (tiePoints > 2 || compScore > 2 || playerScore > 2) {
+        scoreTally();
+    }
+    else if (tiePoints == 2 && compScore == 2 || tiePoints == 2 && playerScore == 2) {
+        scoreTally();
+    }
+    
+    //messes up for some reason
+    // if (tiePoints > 2 || compScore > 2 || playerScore > 2 
+    //     || tiePoints == 2 && compScore == 2 || tiePoints == 2 && playerScore == 2) {
+    //     scoreTally();
+    // }
 
 }
 
-function game() {
-    let compScore = 0,
-        playerScore = 0,
-        tiePoints = 0,
-        playerSelection = ``,
-        result = ``;
-
-    let x = 0
-    while (x < 5) {
-        playerSelection = prompt(`Type in either: Rock, Paper or Scissor`, ``);
-        // if(playerSelection === null || playerSelection == `` || playerSelection == undefined){
-        //     playerSelection = prompt(`Try Again\nType in either: Rock, Paper or Scissor`, ``);
-        // }
-        let points = playRound(playerSelection);
-
-        if (points == `player`) playerScore += 1;
-        else if (points == `comp`) compScore += 1;
-        else tiePoints += 1;
-        console.log(`compScore:${compScore}\nplayerScore:${playerScore}\ntieScore:${tiePoints}\n`);
-
-        if (tiePoints > 2 || compScore > 2 || playerScore > 2) {
-            break;
-        }
-        else if (tiePoints == 2 && compScore == 2 || tiePoints == 2 && playerScore == 2){
-            break;
-        }
-            x++;
-    }
-
+function scoreTally() {
+    let result = ``;
     if (compScore > playerScore) {
         result += `comp wins game`;
     }
@@ -97,7 +108,66 @@ function game() {
     }
     else { result += `tie game, sorry` }
 
-    return console.log(result);
-
+    console.log(result);
+    appendWinner(result);
+    gameEnd();
 }
+
+function gameEnd() {
+    const box = document.getElementById(`endGame`);
+    const btn = document.createElement(`button`);
+    btn.setAttribute(`id`, `replay_btn`);
+    btn.textContent = `Replay?`
+    box.appendChild(btn);
+
+    //reloads page
+    btn.addEventListener(`click`, () => {
+        location.reload();
+    });
+
+    //disables options
+    buttons.forEach((button) => {
+        button.disabled = true;
+    });
+}
+
+function appendRound(text) {
+    const roundDisplay = document.getElementById(`roundDisplay`);
+    roundDisplay.innerText = text;
+}
+
+function appendScore(text) {
+    // const scoreBoard = document.querySelector(`#scoreDisplay`);
+    const scoreDisplay = document.getElementById(`scoreDisplay`);
+    scoreDisplay.innerText = text;
+}
+
+function appendWinner(text) {
+    // const scoreBoard = document.querySelector(`#scoreDisplay`);
+    const winnerDisplay = document.getElementById(`winnerDisplay`);
+    winnerDisplay.innerText = text;
+}
+
+//buttons
+
+// const btn = document.querySelector('#btn');
+// btn.addEventListener('click', function (e) {
+//   console.log(e.target);
+// });
+
+const buttons = document.querySelectorAll(`button`);
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        // console.log(button.id);
+        // console.log(button.innerText);
+        // playRound(button.innerText);
+        game(button.innerText);
+    });
+});
+
+
 
